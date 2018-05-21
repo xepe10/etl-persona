f1b5a8fac9b3254356fc32ecd3f97eeff2257a03
jose cabrera <josecabrera@joses-MacBook-Pro.local>
2018-05-21 15:04:45 -0600

    example rest>csv

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # etl-persona
+
+# About
+
+Nodejs version > 8
+
+# Install dependencies
+
+``` bash
+
+$ npm install
+
+```
+
+Modify CsvWriterMixin to prevent error
+
+/node_modules/datadumps/lib/mixin/CsvWriterMixin.js
+
+On line 26, replace this
+
+``` javascript
+    return target.on('end', function() {
+        return target._csv.writer.write(null);
+    });
+      
+```
+
+With this
+
+``` javascript
+    return target.on('end', function() {
+        return target._csv.writer.end();
+    });
+```
+# Run
+
+``` bash
+
+$ npm run start
+
+```
+
+# Node datadumps
+
+[Official repo](https://github.com/agmen-hu/node-datapumps)
---
+++ index.js
@@ -0,0 +1,38 @@
+var datapump = require('datapumps');
+CsvWriterMixin = datapump.mixin.CsvWriterMixin;
+var pump = new datapump.Pump();
+pump
+    .mixin(datapump.mixin.RestMixin)
+    .from(pump.createBuffer())
+    .get('https://afd662d3.eu.ngrok.io/organization/pais', {
+        "headers":{
+            //Aca va el domain del workspace
+            "wp":"basehcm"
+        },
+        "multipart": false,
+        "query": {
+            //aca van los filtros
+        }
+    })
+    .then(function(result) {
+        //Resultado de llamada a API
+        for(let i in result.result){
+            //Por cada resultado escribir en csv
+            pump.from().write(result.result[i]);
+        }
+        pump.from().seal();
+    });
+
+pump
+    .mixin(CsvWriterMixin({
+        path:'/tmp/ejemplo.csv',
+        headers:['Id', 'Nombre']
+    }))
+    .process(function(result) {
+        return pump.writeRow([ result._id, result.nombre ]);
+    })
+    .logErrorsToConsole()
+    .run()
+    .then(function() {
+        console.log("Done writing to file");
+    });
